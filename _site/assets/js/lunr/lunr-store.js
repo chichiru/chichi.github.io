12d777594e703d0983f13e75065a09dd66dd2100
Chiwon Seo <cldnjs04@korea.ac.kr>
2024-03-13 01:04:15 +0900

    markdown test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Markdown을 이용한 포스트 작성",
+        "excerpt":"제목 1  제목 2  제목 3  ","categories": ["Markdown"],
+        "tags": ["Markdown"],
+        "url": "/markdown/md_test/",
+        "teaser": null
+      },{
         "title": "Post: Video (YouTube)",
         "excerpt":" ","categories": ["Post Formats"],
         "tags": ["Post Formats"],
