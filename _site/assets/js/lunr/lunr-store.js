171bb9de8877e63a39524a7845ec4064aa73c3e7
Chiwon Seo <cldnjs04@korea.ac.kr>
2024-03-13 01:00:41 +0900

    fix some info

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Post: Video (YouTube)",
+        "excerpt":" ","categories": ["Post Formats"],
+        "tags": ["Post Formats"],
+        "url": "/post%20formats/test/",
+        "teaser": null
+      }]
